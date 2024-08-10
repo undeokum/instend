995492d9c7ee2e555a7ad6cd3d521843bae4fbd6
Junseok Lim <limjunseokdev@gmail.com>
2024-08-11 01:46:01 +0900

    20240811

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import * as colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
     },
+    fontFamily: {
+      regular: ['regular'],
+      extra_light: ['extra-light'],
+      light: ['light'],
+      thin: ['thin'],
+      medium: ['medium'],
+      semi_bold: ['semi-bold'],
+      bold: ['bold'],
+      extra_bold: ['extra-bold'],
+      black: ['black'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      red: colors.red,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      slate: colors.slate,
+      orange: colors.orange,
+      blue: colors.blue,
+      instend: '#30B800',
+    }
   },
   plugins: [],
-};
-export default config;
+}
+export default config
